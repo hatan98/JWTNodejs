7b147aba3d4f29388da40799fb6dbc282de0e2d0
Le Pham Ha Tan <tanlephamha@gmail.com>
2024-12-20 10:41:01 +0700

    JWT basic - not refresh token

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
+    'no-debugger': 'on',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import axios from './util/axios.customize'
+import Header from './components/layout/header'
+import { Outlet } from 'react-router-dom'
+import { AuthContext } from './components/context/auth.context.jsx';
 
 function App() {
+  const {setAuth} = useContext(AuthContext)
   const [count, setCount] = useState(0)
-
+  useEffect(()=>{
+    const fetchAccount = async ()=>{
+      const res =await axios.get(`/v1/api/account`);
+       if(res){
+        setAuth({
+          isAuthencontext : true,
+            user :{
+                email: res.data.email,
+                name :res.data.name
+            }
+      })
+      }
+      console.log("check response: " ,res)
+    }
+    fetchAccount()
+  },[])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header/>
+      <Outlet/>
+    </div>
   )
 }
 
